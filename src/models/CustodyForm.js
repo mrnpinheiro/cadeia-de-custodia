3c80b4856e98427ccc0bd39971115cca4949810f
Mariana Pinheiro <mariana.pinheiro@loggi.com>
2021-09-18 22:26:33 -0300

    feat: starting main models

--- src/models/CustodyForm.js
+++ src/models/CustodyForm.js
@@ -1,3 +1,74 @@
-class CustodyForm {
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
+const custodySchema =  new Schema({
+    data_criacao_custodia:{
+        type: Date,
+        default: Date.now
+    },
+    id_novo_lacre:{
+        type: String,
+        required: false
+    },
+    finalidade_lacre:{
+        type: String,
+        required: false
+    },
+    lacre_rompido:{
+        type: String,
+        enum: [
+            'sim',
+            'não',
+        ],
+        required: false
+    },
+    razao:{
+        type: String,
+        required: true
+    },
+    endereco_local_custodia:{
+        latitude_local_custodia:{
+            type: Number,
+            required: false
+        },
+        longitude_local_custodia:{
+            type: Number,
+            required: false
+        },
+        cep_local_custodia:{
+            type: String,
+            required: false
+        },
+        endereco_local_custodia:{
+            type: String,
+            required: false
+        },
+        numero_local_custodia:{
+            type: Number,
+            required: false
+        },
+        cidade_local_custodia:{
+            type: String,
+            required: false
+        },
+        estado_local_custodia:{
+            type: String,
+            required: false
+        }
+    },
+    usuario:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }
+},
+{
+    collection: 'Custody',
+    versionKey: false
+}
+);
+
+const custodyCollection = mongoose.model('Custody', custodySchema)
+
+module.exports = {
+    custodyCollection
 }
