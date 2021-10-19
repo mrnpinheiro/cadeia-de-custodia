bb49b613804a9acf181a9aef73c7ec0a007f9787
RafaelDoering <rafael.doering.soares@usp.br>
2021-10-19 16:39:58 -0300

    feat: add next pwa

---
+++ cadeia-de-custodia-app/pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name='application-name' content='Cadeia de Custodia' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='Cadeia de Custodia' />
+          <meta name='description' content='Cadeia de Custodia' />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='msapplication-TileColor' content='#2B5797' />
+          <meta name='msapplication-tap-highlight' content='no' />
+          <meta name='theme-color' content='#000000' />
+
+          <link rel='icon' type='image/png' sizes='32x32' href='/icons/32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/icons/16.png' />
+          <link rel='manifest' href='/manifest.json' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument;
