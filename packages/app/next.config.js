f12dce0fd154e4a50f9b9f330c2325bd6b3b5e5f
Rafael D <rafaelsoares@tokenlab.com.br>
2021-11-22 13:05:27 -0300

    feat: disable pwa on development stage

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -2,7 +2,8 @@ const withPWA = require('next-pwa');
 
 module.exports = withPWA({
   pwa: {
-    dest: 'out'
+    dest: 'out',
+    disable: process.env.NODE_ENV === 'development'
   },
   reactStrictMode: true,
 });
